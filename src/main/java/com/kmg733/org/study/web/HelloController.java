afc6ede66328ef99c4a60423907291e8253482b3
kmg733 <mang906@naver.com>
2022-03-15 04:36:31 +0900

    JPA With CRUD Done + Unit Test Code

--- src/main/java/com/kmg733/org/study/web/HelloController.java
+++ src/main/java/com/kmg733/org/study/web/HelloController.java
@@ -1,6 +1,7 @@
 package com.kmg733.org.study.web;
 
 import com.kmg733.org.study.web.dto.HelloResponseDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
